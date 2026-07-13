64d8baf7c3803344d38c24569ad5e849094bf017
Daniel Medina <danielmediv@gmail.com>
2026-07-13 10:31:04 -0600

    Creacion de formularios

---
+++ Registro Pacientes/FormHistorialMedico.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace pry_integrador.Registro_Pacientes
+{
+    partial class FormHistorialMedico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHistorialMedico));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Anterior = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Registrar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Nombre = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.Anterior);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.Registrar);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.Nombre);
+            this.groupBox1.Location = new System.Drawing.Point(1, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(774, 845);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label11
+            // 
+            this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label11.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label11.Location = new System.Drawing.Point(64, 642);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(647, 40);
+            this.label11.TabIndex = 61;
+            this.label11.Text = "Al registrar un paciente, confirma que la informacion proporcionada es correcta y" +
+    " completa \r\nde acuerdo con las normativas de la institución";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label5.Location = new System.Drawing.Point(137, 539);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(180, 20);
+            this.label5.TabIndex = 60;
+            this.label5.Text = "Antecedentes familiares";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 562);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 26);
+            this.textBox1.TabIndex = 59;
+            // 
+            // Anterior
+            // 
+            this.Anterior.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Anterior.ForeColor = System.Drawing.SystemColors.Control;
+            this.Anterior.Location = new System.Drawing.Point(105, 759);
+            this.Anterior.Name = "Anterior";
+            this.Anterior.Size = new System.Drawing.Size(110, 40);
+            this.Anterior.TabIndex = 58;
+            this.Anterior.Text = "Anterior";
+            this.Anterior.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label7.Location = new System.Drawing.Point(338, 769);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 20);
+            this.label7.TabIndex = 57;
+            this.label7.Text = "Paso 3 de 3";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // Registrar
+            // 
+            this.Registrar.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Registrar.ForeColor = System.Drawing.SystemColors.Control;
+            this.Registrar.Location = new System.Drawing.Point(559, 759);
+            this.Registrar.Name = "Registrar";
+            this.Registrar.Size = new System.Drawing.Size(110, 40);
+            this.Registrar.TabIndex = 56;
+            this.Registrar.Text = "Registrar";
+            this.Registrar.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(151, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(74, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 55;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(223, 91);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(416, 20);
+            this.label9.TabIndex = 54;
+            this.label9.Text = "Información clinica relevante para la atencion del paciente";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(222, 62);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(203, 29);
+            this.label8.TabIndex = 53;
+            this.label8.Text = "Historial Medico";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label6.Location = new System.Drawing.Point(135, 391);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(177, 20);
+            this.label6.TabIndex = 52;
+            this.label6.Text = "Medicamentos actuales";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(135, 463);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 20);
+            this.label4.TabIndex = 51;
+            this.label4.Text = "Cirugias previas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(139, 321);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 20);
+            this.label3.TabIndex = 50;
+            this.label3.Text = "Condiciones cronicas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(135, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "Tipo de sangre";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "O-",
+            "O+",
+            "A-",
+            "A+",
+            "B-",
+            "B+",
+            "AB-",
+            "AB+"});
+            this.comboBox2.Location = new System.Drawing.Point(139, 201);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(161, 28);
+            this.comboBox2.TabIndex = 48;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(139, 344);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(294, 26);
+            this.textBox4.TabIndex = 47;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(137, 486);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(294, 26);
+            this.textBox3.TabIndex = 46;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(139, 271);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(294, 26);
+            this.textBox2.TabIndex = 45;
+            // 
+            // Nombre
+            // 
+            this.Nombre.Location = new System.Drawing.Point(139, 414);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(292, 26);
+            this.Nombre.TabIndex = 44;
+            // 
+            // FormHistorialMedico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 844);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormHistorialMedico";
+            this.Text = "FormHistorialMedico";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Anterior;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button Registrar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox Nombre;
+    }
+}
---
+++ Registro de Usuarios/FormRegistroUsers.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace pry_integrador.Registro_de_Usuarios
+{
+    partial class FormRegistroUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.buttonRegistrar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textConfPassword = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.labelPerfil = new System.Windows.Forms.Label();
+            this.labelConfContraseña = new System.Windows.Forms.Label();
+            this.labelContraseña = new System.Windows.Forms.Label();
+            this.labelNombreUsuario = new System.Windows.Forms.Label();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonCancelar);
+            this.groupBox1.Controls.Add(this.buttonRegistrar);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textConfPassword);
+            this.groupBox1.Controls.Add(this.textPassword);
+            this.groupBox1.Controls.Add(this.labelPerfil);
+            this.groupBox1.Controls.Add(this.labelConfContraseña);
+            this.groupBox1.Controls.Add(this.labelContraseña);
+            this.groupBox1.Controls.Add(this.labelNombreUsuario);
+            this.groupBox1.Controls.Add(this.textNombre);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(479, 345);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registro de Nuevo Usuario";
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(371, 263);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(87, 33);
+            this.buttonCancelar.TabIndex = 19;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // buttonRegistrar
+            // 
+            this.buttonRegistrar.Location = new System.Drawing.Point(218, 263);
+            this.buttonRegistrar.Name = "buttonRegistrar";
+            this.buttonRegistrar.Size = new System.Drawing.Size(94, 33);
+            this.buttonRegistrar.TabIndex = 18;
+            this.buttonRegistrar.Text = "Registrar";
+            this.buttonRegistrar.UseVisualStyleBackColor = true;
+            this.buttonRegistrar.Click += new System.EventHandler(this.buttonRegistrar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Medico",
+            "Enfermera"});
+            this.comboBox1.Location = new System.Drawing.Point(218, 203);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(240, 30);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // textConfPassword
+            // 
+            this.textConfPassword.Location = new System.Drawing.Point(218, 149);
+            this.textConfPassword.Name = "textConfPassword";
+            this.textConfPassword.Size = new System.Drawing.Size(240, 28);
+            this.textConfPassword.TabIndex = 16;
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(218, 97);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(240, 28);
+            this.textPassword.TabIndex = 15;
+            // 
+            // labelPerfil
+            // 
+            this.labelPerfil.AutoSize = true;
+            this.labelPerfil.Location = new System.Drawing.Point(22, 203);
+            this.labelPerfil.Name = "labelPerfil";
+            this.labelPerfil.Size = new System.Drawing.Size(58, 22);
+            this.labelPerfil.TabIndex = 14;
+            this.labelPerfil.Text = "Perfil:";
+            // 
+            // labelConfContraseña
+            // 
+            this.labelConfContraseña.AutoSize = true;
+            this.labelConfContraseña.Location = new System.Drawing.Point(21, 149);
+            this.labelConfContraseña.Name = "labelConfContraseña";
+            this.labelConfContraseña.Size = new System.Drawing.Size(184, 22);
+            this.labelConfContraseña.TabIndex = 13;
+            this.labelConfContraseña.Text = "Confirmar Contrseña:";
+            // 
+            // labelContraseña
+            // 
+            this.labelContraseña.AutoSize = true;
+            this.labelContraseña.Location = new System.Drawing.Point(21, 97);
+            this.labelContraseña.Name = "labelContraseña";
+            this.labelContraseña.Size = new System.Drawing.Size(106, 22);
+            this.labelContraseña.TabIndex = 12;
+            this.labelContraseña.Text = "Contraseña:";
+            // 
+            // labelNombreUsuario
+            // 
+            this.labelNombreUsuario.AutoSize = true;
+            this.labelNombreUsuario.Location = new System.Drawing.Point(22, 51);
+            this.labelNombreUsuario.Name = "labelNombreUsuario";
+            this.labelNombreUsuario.Size = new System.Drawing.Size(175, 22);
+            this.labelNombreUsuario.TabIndex = 11;
+            this.labelNombreUsuario.Text = "Nombre de Usuario:";
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(218, 48);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(240, 28);
+            this.textNombre.TabIndex = 10;
+            // 
+            // FormRegistroUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 344);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormRegistroUsers";
+            this.Text = "FormRegistroUsers";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonCancelar;
+        private System.Windows.Forms.Button buttonRegistrar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textConfPassword;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label labelPerfil;
+        private System.Windows.Forms.Label labelConfContraseña;
+        private System.Windows.Forms.Label labelContraseña;
+        private System.Windows.Forms.Label labelNombreUsuario;
+        private System.Windows.Forms.TextBox textNombre;
+    }
+}
