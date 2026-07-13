2d0cf87d6576077f5e36f967da78bcd2de8bba54
Ricardo-Jesus77 <20250990@uthh.edu.mx>
2026-07-13 12:29:49 -0600

    cambios

--- FormPrincipal.Designer.cs
+++ FormPrincipal.Designer.cs
@@ -229,18 +229,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(86, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 48);
+            this.label1.Size = new System.Drawing.Size(119, 75);
             this.label1.TabIndex = 1;
             this.label1.Text = "MediAgenda\r\n\r\nCita Medicas\r\n";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(1669, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 32);
+            this.label2.Size = new System.Drawing.Size(97, 50);
             this.label2.TabIndex = 2;
             this.label2.Text = "Enfermera\r\nRecepcion\r\n";
             // 
